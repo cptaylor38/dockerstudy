d6d67c7236b5030152a2617e239e63919cd117e7
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-10 18:50:08 -0500

    will assign this to puppeteer study for sweepstakes entry, not feeling well and need to do more research on puppeteer first

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const puppeteer = require('puppeteer');
 
 app.get('/', (req,res)=> {
     res.send('Hello World');
 });
+
+app.get('/hgtv', (req, res)=> {
+
+    let action = ( async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.hgtv.com/sweepstakes/hgtv-dream-home/sweepstakes');
+    })();
+
+    res.send(action);
+})
 
 app.listen(8080, ()=> {
     console.log('Listening on Port 8080');
