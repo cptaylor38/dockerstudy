a10b4256ac2a199b51d39e421f92f7c6e5bc24da
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-11 10:48:54 -0500

    turning this exercise into a back end for my pharmacy training module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dockerstudy
+
+Using this as a way to study docker. Will implement this as a back-end for Wag training module.
--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const app = express();
-const puppeteer = require('puppeteer');
 
 app.get('/', (req,res)=> {
     res.send('Hello World');
 });
 
-app.get('/hgtv', (req, res)=> {
-
-    let action = ( async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.hgtv.com/sweepstakes/hgtv-dream-home/sweepstakes');
-    })();
-
-    res.send(action);
+app.get('/storenet', (req, res)=> {
+    res.send('launching');
 })
 
-app.listen(8080, ()=> {
-    console.log('Listening on Port 8080');
+app.get('/icplus', (req, res)=> {
+    res.send('icplus')
+})
+
+app.get('patientprofile', (req, res)=> {
+    res.send('patientprofile')
+})
+
+app.get('thirdparty', (req, res)=> {
+    res.send('thirdparty');
+})
+
+app.get('sdl', (req, res)=> {
+    res.send('sdl');
+})
+
+app.listen(8000, ()=> {
+    console.log('Listening on Port 8000');
 })
